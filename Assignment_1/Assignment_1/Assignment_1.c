4e339b8d47d4bafa2e231f44f0f109e6ad90c475
maratyuriev25-pixel <marat.yuriev.25@kse.org.ua>
2026-05-20 21:16:49 +0300

    Starting console app

--- Assignment_1/Assignment_1/Assignment_1.c
+++ Assignment_1/Assignment_1/Assignment_1.c
@@ -2,6 +2,21 @@
 
 int main(void)
 {
-    printf("Hello\n");
+    char text[] = "";
+    int user_input;
+    printf(
+        "Choose the command: \n"
+        "1.Enter text to append\n"
+        "2.Start the new line\n"
+        "3.Use files to load/save the information\n"
+        "4.Print the current text to console\n"
+        "5.Insert the text by line and symbol index\n"
+        "6.Search\n"
+        "7.Clearing the console\n");
+
+   scanf("%d", &user_input);
+
+   printf("You entered: %d\n", user_input);
+
     return 0;
 }
