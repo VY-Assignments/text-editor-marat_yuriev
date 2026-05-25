e3b2a6fc5c2a17f1b1ff2c50910b35c1f4633428
maratyuriev25-pixel <marat.yuriev.25@kse.org.ua>
2026-05-25 12:31:30 +0300

    Int main refactoring and File logic complete

--- Assignment_1/Assignment_1/Assignment_1.c
+++ Assignment_1/Assignment_1/Assignment_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <string.h>
 // ================================ Linked List ================================
 
 typedef struct Node
@@ -46,6 +46,21 @@ void AppendNode(Node* newNode)
     current->next = newNode;
 }
 
+void FreeList()
+{
+    Node* current = head;
+
+    while (current != NULL)
+    {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    head = NULL;
+}
+
+
 // ================================ Task 1 ================================
 
 void Append(char user_text[])
@@ -70,47 +85,149 @@ void StartNewLine()
     AppendNode(CreateNode('\n'));
 }
 
+// ================================ Task 3 ================================ 
+
+void SaveInfo(const char filename[])
+{
+    FILE* file = fopen(filename, "w");
+
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return;
+    }
+
+    Node* current = head;
+
+    while (current != NULL)
+    {
+        fputc(current->x, file);
+        current = current->next;
+    }
+
+    fclose(file);
+
+    printf("Text saved successfully\n");
+}
+
+
+// ================================ Task 4 ================================
+
+void LoadFile(const char filename[])
+{
+    FILE* file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return;
+    }
+
+    FreeList();
+
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        AppendNode(CreateNode(ch));
+    }
+
+    fclose(file);
+
+    printf("Text loaded successfully\n");
+}
+
+// ================================ Task 5 ================================
+
+void Print()
+{
+    Node* current = head;
+
+    while (current != NULL)
+    {
+        printf("%c", current->x);
+        current = current->next;
+    }
+
+    printf("\n");
+}
+
+// ================================ Task 6 ================================
+
+void InsertLine()
+{
+
+}
+
 
 //================================ Main Function ================================
 
 int main(void)
 {
-    char user_input[100];
+    char user_input[1000];
+    char filename[100];
     int user_choice;
-
-    while(true) 
+    printf(
+        "Choose the command(Enter 1 or 2...7): \n"
+        "1.Enter text to append\n"
+        "2.Start the new line\n"
+        "3.Use files to save the information\n"
+        "4.Use files to load the information\n"
+        "5.Print the current text to console\n"
+        "6.Insert the text by line and symbol index\n"
+        "7.Search\n"
+        "8.Clearing the console\n");
+    while (true)
     {
-        printf(
-            "Choose the command(Enter 1 or 2...7): \n"
-            "1.Enter text to append\n"
-            "2.Start the new line\n"
-            "3.Use files to load/save the information\n"
-            "4.Print the current text to console\n"
-            "5.Insert the text by line and symbol index\n"
-            "6.Search\n"
-            "7.Clearing the console\n");
-
         scanf_s("%d", &user_choice);
         while (getchar() != '\n');
 
-        switch (user_choice)
-        {   
+            switch (user_choice)
+            {
+            case 1:
+                printf("Write text to append:\n");
+                fgets(user_input, sizeof(user_input), stdin);
+                Append(user_input);
+                break;
 
-        case(1):
-            printf("Write text which you want to append\n");
-            fgets(user_input, sizeof(user_input), stdin);
-            Append(user_input);
-            break;
+            case 2:
+                StartNewLine();
+                Printf("A new line was added");
+                break;
 
-        case(2):
-            StartNewLine();
-            break;
+            case 3:
+                printf("Enter file name to save:\n");
+                fgets(filename, sizeof(filename), stdin);
+                filename[strcspn(filename, "\r\n")] = 0;
+                SaveInfo(filename);
+                break;
 
-        default:
-            printf("Wrong Command\n");
-            break;
-        }
+            case 4:
+                printf("Enter file name to load:\n");
+                fgets(filename, sizeof(filename), stdin);
+                filename[strcspn(filename, "\r\n")] = 0;
+                LoadFile(filename);
+                break;
 
+            case 5:
+                Print();
+                break;
+
+            case 6:
+                InsertLine();
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                FreeList();
+                return 0;
+
+            default:
+                printf("Wrong command\n");
+                break;
+            }
     }
-    
+        
 }
